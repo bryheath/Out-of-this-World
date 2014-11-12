2f3cd9035d1fe72eaf08494ce6a916193acd7855
Sunrai <bryheath86@gmail.com>
2014-11-12 12:31:24 -0600

    Section 30 complete

---
+++ Out of this World/Model/SpaceObject.m
@@ -0,0 +1,13 @@
+//
+//  SpaceObject.m
+//  Out of this World
+//
+//  Created by Bryan Heath on 11/12/14.
+//  Copyright (c) 2014 Bryan Heath. All rights reserved.
+//
+
+#import "SpaceObject.h"
+
+@implementation SpaceObject
+
+@end
