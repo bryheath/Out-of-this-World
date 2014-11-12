ca49641aa4f3a3f8b3e02b629ecab39f190f9270
Sunrai <bryheath86@gmail.com>
2014-11-12 10:19:00 -0600

    first commit

---
+++ Out of this World/ViewControllers/OWOuterSpaceTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  OWOuterSpaceTableViewController.h
+//  Out of this World
+//
+//  Created by Bryan Heath on 11/5/14.
+//  Copyright (c) 2014 Bryan Heath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OWOuterSpaceTableViewController : UITableViewController
+
+@end
